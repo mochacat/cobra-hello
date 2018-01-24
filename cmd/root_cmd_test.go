bd03a6f4afe2657b1cb7469887dc6b76445783cb
Catherine Moca <12579725+mochacat@users.noreply.github.com>
2018-01-24 03:02:10 -0500

    test cases

--- cmd/root_cmd_test.go
+++ cmd/root_cmd_test.go
@@ -2,17 +2,26 @@ package cmd
 
 import (
     "bytes"
-    "fmt"
     "testing"
 )
 
 func TestSayHello(t *testing.T) {
-    bak := out
-    out = new(bytes.Buffer)
-    defer func() { out = bak }()
-    n := name{"Jerk"}
-    n.printName()
-    if out.(*bytes.Buffer).String() == "Hello, Jerk!\n" {
-        fmt.Println("Works as expected")
+    cases := []struct {
+        name, want string
+    }{
+        {"Cat", "Hello, Cat!\n"},
+        {"", "Hello, !\n"},
+        {"Mr. Fancy Pants", "Hello, Mr. Fancy Pants!\n"},
+    }
+    for _, c := range cases {
+        bak := out
+        out = new(bytes.Buffer)
+        defer func() { out = bak }()
+        n := name{c.name}
+        n.printName()
+        printedName := out.(*bytes.Buffer).String()
+        if printedName != c.want {
+            t.Errorf("Wanted %q, Got %q", c.want, printedName)
+        }
     }
 }
